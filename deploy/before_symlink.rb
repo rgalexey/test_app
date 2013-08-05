c13849428d201c1ed7f2cf96d3d4eead8cae9f6d
rgalexey <aostrovsky@rightandabove.com>
2013-08-05 15:00:03 +0300

    test

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,15 +1,16 @@
 Chef::Log.info("Running deploy/before_symlink.rb...")
 run "cd #{release_path} && RAILS_ENV=production bundle exec rake assets:precompile"
 
-case node["platform"]
+case node['platform']
 when "debian", "ubuntu"
   # do debian/ubuntu things
 when "redhat", "centos", "fedora"
  #   # do redhat/centos/fedora things
 end
 
+Chef::Log.info(node['platform'])
 
-      case node[:ec2][:instance_type]
+      case node['ec2']['instance_type']
       when 'm1.small' then worker_count = 2
       when 'c1.medium' then worker_count = 4
       when 'c1.xlarge' then worker_count = 8
@@ -17,5 +18,5 @@ end
         worker_count = 2
       end
 
-Chef::Log.info(node[:ec2][:instance_type])
+Chef::Log.info(node['ec2']['instance_type'])
 Chef::Log.info(worker_count)
