2037eeee52c41f0ee158cf9458ba86964c887d7d
rgalexey <aostrovsky@rightandabove.com>
2013-08-05 14:29:55 +0300

    testing

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,14 +1,9 @@
 Chef::Log.info("Running deploy/before_symlink.rb...")
 run "cd #{release_path} && RAILS_ENV=production bundle exec rake assets:precompile"
 
-package "apache2" do
-  case node[:platform]
-  when "centos","redhat","fedora","suse"
-    package_name "httpd"
-  when "debian","ubuntu"
-    package_name "apache2"
-  when "arch"
-    package_name "apache"
-  end
-  action :install
+case node["platform"]
+when "debian", "ubuntu"
+  # do debian/ubuntu things
+when "redhat", "centos", "fedora"
+ #   # do redhat/centos/fedora things
 end
