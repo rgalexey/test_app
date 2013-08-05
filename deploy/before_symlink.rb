fa7b4af1f6c75490719d8c7a2d0e4c4636f0be99
rgalexey <aostrovsky@rightandabove.com>
2013-08-05 14:54:32 +0300

    testing

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -9,7 +9,7 @@ when "redhat", "centos", "fedora"
 end
 
 
-      case node[ec2][instance_type]
+      case node[:ec2][:instance_type]
       when 'm1.small' then worker_count = 2
       when 'c1.medium' then worker_count = 4
       when 'c1.xlarge' then worker_count = 8
@@ -17,5 +17,5 @@ end
         worker_count = 2
       end
 
-Chef::Log.info(node[ec2][instance_type])
+Chef::Log.info(node[:ec2][:instance_type])
 Chef::Log.info(worker_count)
