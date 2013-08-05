e4f1f707d0237780cb58b2ebea70f903c5fe9452
rgalexey <aostrovsky@rightandabove.com>
2013-08-05 17:00:40 +0300

    back

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,6 @@
 # Bind release_path to local variable
-cache_release_path = release_path
-opsworks_app_environment do
-  app_name 'test_app'
-  release_path cache_release_path
-end
+#cache_release_path = release_path
+#opsworks_app_environment do
+#  app_name 'test_app'
+#  release_path cache_release_path
+#end
