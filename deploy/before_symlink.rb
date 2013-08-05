de31f55e701972aa2d8aaed65e2503112c6d77fb
rgalexey <aostrovsky@rightandabove.com>
2013-08-05 14:25:50 +0300

    testing

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,16 +1,14 @@
 Chef::Log.info("Running deploy/before_symlink.rb...")
 run "cd #{release_path} && RAILS_ENV=production bundle exec rake assets:precompile"
 
-    if node[:instance_role] == 'solo'
-      worker_count = 1
-    else
-      case node[:ec2][:instance_type]
-      when 'm1.small' then worker_count = 2
-      when 'c1.medium' then worker_count = 4
-      when 'c1.xlarge' then worker_count = 8
-      else
-        worker_count = 2
-      end
-    end
-
-Chef::Log.info(worker_count);
+package "apache2" do
+  case node[:platform]
+  when "centos","redhat","fedora","suse"
+    package_name "httpd"
+  when "debian","ubuntu"
+    package_name "apache2"
+  when "arch"
+    package_name "apache"
+  end
+  action :install
+end
