4223be0c0207eeea88d5b517d964f750e0248a54
rgalexey <aostrovsky@rightandabove.com>
2013-08-23 16:38:51 +0300

    Working on delayed jobs hooks

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -2,7 +2,7 @@ Chef::Log.info("Running deploy/before_migrate.rb...")
 
 Chef::Log.info(node[:opsworks][:instance][:layers].to_s)
 
-  if node[:opsworks][:instance][:layers].to_s =~ /custom/
+  if node[:opsworks][:instance][:layers].to_s =~ /app-layer/
         Chef::Log.debug("CUSTOM_DEBUG: Instance belongs to delayed proc layer. Will run delayed proc handler")
         Chef::Log.debug("CUSTOM_DEBUG: cd #{release_path} && RAILS_ENV=production script/delayed_jobs_stop")
         Chef::Log.info(`cd #{release_path} && RAILS_ENV=production script/delayed_jobs_stop`)
