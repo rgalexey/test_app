4e598fb477ae57d89ac6ae5a5f4b9d6be863b3f9
rgalexey <aostrovsky@rightandabove.com>
2013-08-23 16:36:38 +0300

    Working on delayed jobs hooks

---
+++ deploy/after_restart.rb
@@ -0,0 +1,12 @@
+Chef::Log.info("Running deploy/after_restart.rb...")
+
+
+
+Chef::Log.info(node[:opsworks][:instance][:layers].to_s)
+
+ if node[:opsworks][:instance][:layers].to_s =~ /custom/
+        Chef::Log.debug("CUSTOM_DEBUG: Instance belongs to delayed proc layer. Will run delayed proc handler")
+        Chef::Log.debug("CUSTOM_DEBUG: cd #{release_path} && RAILS_ENV=production script/delayed_jobs_start")
+        Chef::Log.info(`cd #{release_path} && RAILS_ENV=production script/delayed_jobs_start`) 
+  end
+
