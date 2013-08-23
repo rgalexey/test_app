17706cca24a1f99500f5f6c0a0b244fd4aa440e7
rgalexey <aostrovsky@rightandabove.com>
2013-08-23 16:06:57 +0300

    Working on delayed jobs hooks

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -9,8 +9,11 @@ end
 
 Chef::Log.info(node['platform'])
 Chef::Log.info(node[:opsworks][:instance][:instance_type])
+Chef::Log.info(node[:opsworks][:instance][:hostname])
 
-Chef::Log.info("[ -e #{node[:deploy][:rg_api_dly_proc][:deploy_to]}/current/ ] && ( echo 'We are on delayed job box. Stopping delayed jobs'; cd #{node[:deploy][:rg_api_dly_proc][:deploy_to]}/current/ && RAILS_ENV=production script/delayed_jobs_stop )")
+Chef::Log.info(node[:deploy][application][:name])
+
+#Chef::Log.info("[ -e #{node[:deploy][:rg_api_dly_proc][:deploy_to]}/current/ ] && ( echo 'We are on delayed job box. Stopping delayed jobs'; cd #{node[:deploy][:rg_api_dly_proc][:deploy_to]}/current/ && RAILS_ENV=production script/delayed_jobs_stop )")
 #Chef::Log.info(`[ -e #{node[:deploy][:rg_api_dly_proc][:deploy_to]}/current/ ] && ( echo 'We are on delayed job box. Stopping delayed jobs'; cd #{node[:deploy][:rg_api_dly_proc][:deploy_to]}/current/ && RAILS_ENV=production script/delayed_jobs_stop )`)
 
 #      case node['ec2']['instance_type']
